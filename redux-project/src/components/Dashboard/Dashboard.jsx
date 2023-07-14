49ca7a4168f14a4b043962b8e87b3c8db5ebd2d6
dondifer <dondifer@gmail.com>
2023-07-14 20:02:25 +0200

    feat: dashboard

--- redux-project/src/components/Dashboard/Dashboard.jsx
+++ redux-project/src/components/Dashboard/Dashboard.jsx
@@ -1,10 +1,14 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAll } from "../../features/posts/postsSlice";
+import { Spin } from "antd";
 
+import { LoadingOutlined } from "@ant-design/icons";
 import { Card, Skeleton } from "antd";
 import Posts from "../Posts/Posts";
 const { Meta } = Card;
+
+const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
 
 const Dashboard = () => {
   const dispatch = useDispatch();
@@ -18,16 +22,12 @@ const Dashboard = () => {
     <div>
       {isLoading ? (
         <>
-          <Card style={{ marginTop: 8 }} actions={[]}>
+          <Card style={{ marginTop: 16 }} actions={[]}>
             <Skeleton loading={isLoading} active>
               <Meta title="Card title" description="This is the description" />
             </Skeleton>
           </Card>
-          <Card style={{ marginTop: 8 }} actions={[]}>
-            <Skeleton loading={isLoading} active>
-              <Meta title="Card title" description="This is the description" />
-            </Skeleton>
-          </Card>
+          <Spin indicator={antIcon} />
         </>
       ) : (
         <>
