cccb494588d6cc87babf86c0c67fc0a71a509d2a
dondifer <dondifer@gmail.com>
2023-07-17 19:32:33 +0200

    feat: make new post form

--- redux-project/src/components/AddNew/AddNew.jsx
+++ redux-project/src/components/AddNew/AddNew.jsx
@@ -1,7 +1,57 @@
 import React from "react";
+import { Button, Form, Input, Select } from "antd";
 
 const AddNew = () => {
-  return <div>AddNew</div>;
+  const formRef = React.useRef(null);
+  const onFinish = (values) => {
+    console.log(values);
+  };
+  const onReset = () => {
+    formRef.current?.resetFields();
+  };
+  return (
+    <div>
+      <Form
+        ref={formRef}
+        name="control-ref"
+        onFinish={onFinish}
+        style={{
+          maxWidth: 600,
+        }}
+      >
+        <Form.Item
+          name="title"
+          label="Title"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name="description"
+          label="Description"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item className="button-layout">
+          <Button type="primary" htmlType="submit">
+            Post
+          </Button>
+          <Button htmlType="button" onClick={onReset}>
+            Reset
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
 };
 
 export default AddNew;
